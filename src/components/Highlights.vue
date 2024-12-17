ee7e7d86427c930f5bd200d0418cc68c0fd6f54c
botOliver <botoliver556@gmail.com>
2024-12-17 21:58:25 +0300

    Получение информации по API v.3 17.12.2024

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -1,8 +1,18 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  weatherInfo: {
+    type: [Object, null],
+    required: true
+  }
+})
+
+import {getPressureMm} from '../utils/index'
+
+</script>
 
 <template>
-	<div class="section highlights">
-		<div class="title">Today's Highlights</div>
+	<div v-if="weatherInfo?.weather" class="section highlights">
+		<div class="title">"Today's Highlights"</div>
 		<div class="highlights-wrapper">
 			<div class="highlight">
 				<div class="card">
@@ -11,11 +21,11 @@
 					<div class="card-info">
 						<div class="card-justify">
 							<div class="info-main">
-								<div class="info-main-num">3.6</div>
+								<div class="info-main-num">{{weatherInfo?.wind?.speed}}</div>
 								<div class="info-main-text">m/s</div>
 							</div>
 							<div class="info-main">
-								<div class="info-main-num">350</div>
+								<div class="info-main-num">{{weatherInfo?.wind?.deg}}</div>
 								<div class="info-main-text">deg</div>
 							</div>
 						</div>
@@ -24,8 +34,8 @@
 				<div class="card-small">
 					<div class="card-small-title">Wind gusts</div>
 					<div class="card-small-info">
-						<div class="card-small-data">
-							<div class="info-main-num">8.4</div>
+						<div v-if="weatherInfo?.wind?.gust" class="card-small-data">
+							<div class="info-main-num">{{Math.round(weatherInfo?.wind?.gust)}}</div>
 							<div class="info-main-text">m/s</div>
 						</div>
 						<div class="card-small-hint">
@@ -50,7 +60,7 @@
 					<div class="card-info">
 						<div class="card-centered">
 							<div class="info-main">
-								<div class="info-main-num">765</div>
+								<div class="info-main-num">{{getPressureMm(weatherInfo?.main?.pressure)}}</div>
 								<div class="info-main-text">mm</div>
 							</div>
 						</div>
@@ -60,7 +70,7 @@
 					<div class="card-small-title">Feels like</div>
 					<div class="card-small-info">
 						<div class="card-small-data">
-							<div class="info-main-num">21</div>
+							<div class="info-main-num">{{Math.round(weatherInfo?.main?.feels_like)}}</div>
 							<div class="info-main-text">°C</div>
 						</div>
 						<div class="card-small-hint">
@@ -95,7 +105,7 @@
 					<div class="card-small-title">Cloudiness</div>
 					<div class="card-small-info">
 						<div class="card-small-data">
-							<div class="info-main-num">80</div>
+							<div class="info-main-num">{{weatherInfo?.clouds?.all}}</div>
 							<div class="info-main-text">%</div>
 						</div>
 						<div class="card-small-hint">
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,9 @@ export const capitalizeFirstLetter = (str)=>{
 
     return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+const PRESSURE_UNITS = 0.750063
+
+export const getPressureMm = (hpa)=>{
+    return Math.round(hpa*PRESSURE_UNITS)
+}
